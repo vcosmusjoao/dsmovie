ab202e87b7210833593bfe27eaab0f364776e8a0
João Victor Costa Santos <rm88372@fiap.com.br>
2022-03-12 12:42:49 -0300

    Props.

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,22 +1,32 @@
 import axios from "axios";
-import { useState } from "react";
-import { MoviePage } from "types/movie";
+import { useEffect, useState } from "react";
+import { Movie, MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+    const [pageNumber, setPageNumber] = useState(0); //valor inicial da página é 0 que vai ser setado ai na URL
 
-
-    const[pageNumber, setPageNumber] = useState(0); //valor inicial da página é 0 
-    //forma errada
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response =>{
-        const data = response.data as MoviePage;
-        setPageNumber(data.number);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
     });
 
-
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
 
 
 
@@ -27,25 +37,14 @@ function Listing() {
 
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </>
